847eff0b3931cd7c00a0d84b55577ac1173398c4
aizazhussain46 <aizaz.hussain@orangeroomdigital.com>
2020-07-06 11:49:16 +0500

    dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -1,81 +1,45 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
+  <v-layout column>
+
+  <v-row>
+
+
+      <v-col  cols="4">
+          <v-card
+            color="teal"
+            dark
           >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
+            <v-card-title class="headline">Doctors</v-card-title>
+            <v-card-actions>
+              <v-btn text>{{dl}}</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col  cols="4">
+          <v-card
+            color="teal"
+            dark
           >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
+            <v-card-title class="headline">Cases</v-card-title>
+            <v-card-actions>
+              <v-btn text>{{pt}}</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col  cols="4">
+          <v-card
+            color="teal"
+            dark
           >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
+            <v-card-title class="headline">Field Officers</v-card-title>
+            <v-card-actions>
+              <v-btn text>{{fo}}</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+ 
+  
   </v-layout>
 </template>
 
@@ -87,6 +51,52 @@ export default {
   components: {
     Logo,
     VuetifyLogo
+  },
+  data (){
+    return {
+      dl:'',
+      pt:'',
+      fo:'',
+        items: [
+        // {    
+         
+        //   title: 'Doctors',
+        //   total: '05',
+        // },
+        // {    
+         
+        //   title: 'Doctors',
+        //   total: '05',
+        // },
+        // {    
+         
+        //   title: 'Doctors',
+        //   total: '05',
+        // },
+      
+      ],
+    }
+  },
+  created(){
+
+   this.$axios.get('doctors').then(res => {
+
+          this.dl = res.data.data.length
+  
+        });
+
+  this.$axios.get('patient').then(res => {
+
+          this.pt = res.data.length
+  
+        });  
+
+  this.$axios.get('field_officers').then(res => {
+
+          this.fo = res.data.data.length
+  
+        });
+
   }
 }
 </script>
